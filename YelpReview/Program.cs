5aa7c2f5237f245feaae5f45f70991b3a53e17f8
Richard Wooters <wooters.richard@gmail.com>
2023-01-24 23:37:51 -0800

    added tests for Phrases.

--- YelpReview/Program.cs
+++ YelpReview/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Threading.Tasks;
+using Textclassification; 
 using YelpReview.Models;
 using static VO.Con;
 
@@ -13,14 +15,34 @@ namespace YelpReview
 		static void Main(string[] args)
 		{
 			int i = 0;
+			Textclassification.Classifier cFier = new Textclassification.Classifier();
 			YelpDataSource yr = new YelpDataSource();
 			foreach (YelpModel review in yr.GetAllYelpReviews() )
 			{
-				QOut(review.TypeReview, review.ReviewText);
 				i++;
-				if (i > 20)
-					break;
-
+				if (i < 500)
+				{
+					QOut(review.TypeReview, review.ReviewText);
+					cFier.TeachPhrases(review.TypeReview, review.ReviewText);
+					continue;
+				}	
+				
+				if(i >= 500 && i < 750)
+				{
+					var result = cFier.FindStatCategory(review.ReviewText);
+					if(!result.StartsWith(review.TypeReview))
+					{
+						QOut("Failed !!!!");
+						QOut(result, review.TypeReview);
+					}
+					else
+					{
+						QOut("Matched ");
+					}
+					QOut("----------------");
+					continue;
+				}
+				break;
 			}
 			//QOut("Hello y-all");
 			QOut();
